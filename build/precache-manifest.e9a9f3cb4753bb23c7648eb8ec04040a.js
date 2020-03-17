60e1019e5db130c3d8bd94983bcd2da2b9cf630e
Deeamon <marinasugutova@gmail.com>
2020-03-17 23:44:08 +0300

    add: build

---
+++ build/precache-manifest.e9a9f3cb4753bb23c7648eb8ec04040a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1d4daf4131b6d023fd16e2e922794bc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "64e75eef04798dda50b5",
+    "url": "./static/css/main.157be5be.chunk.css"
+  },
+  {
+    "revision": "eb1e2dcb90d590ad361d",
+    "url": "./static/js/2.1046d7dd.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "./static/js/2.1046d7dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64e75eef04798dda50b5",
+    "url": "./static/js/main.bc79a8a9.chunk.js"
+  },
+  {
+    "revision": "e6398e6ef302edff7b00",
+    "url": "./static/js/runtime-main.23a781e4.js"
+  }
+]);
